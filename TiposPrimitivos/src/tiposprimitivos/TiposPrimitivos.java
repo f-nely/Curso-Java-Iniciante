a07afd2bb2bed666985af50af0acb77b5a73a65e
nely de faria <nelly.nf54@gmail.com>
2020-10-16 11:33:26 -0300

    Adiciona classe TiposPrimitivos

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+
+package tiposprimitivos;
+
+/**
+ *
+ * @author Nely
+ */
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        float nota = 8.5f;
+        
+        System.out.println("A nota é: " +nota);
+    }
+    
+}
