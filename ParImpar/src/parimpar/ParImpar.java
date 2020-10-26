c654803d2ab2f063e78d5e29aaad2dc5c261f7da
nely de faria <nelly.nf54@gmail.com>
2020-10-26 17:06:36 -0300

    Adiciona classe ParImpar

---
+++ ParImpar/src/parimpar/ParImpar.java
@@ -0,0 +1,23 @@
+package parimpar;
+
+import java.util.Scanner;
+
+public class ParImpar {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 2; i++) {
+            System.out.print("Informe o número: ");
+            int numero = scan.nextInt();
+
+            if (numero % 2 == 0) {
+                System.out.println("Par: ");
+            } else {
+                System.out.println("Ímpar: ");
+            }
+        }
+
+    }
+
+}
