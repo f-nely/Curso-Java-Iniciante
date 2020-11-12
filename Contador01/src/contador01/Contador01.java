78f318086a57233b25ba508d4c0047aaeb7fa8b6
nely de faria <nelly.nf54@gmail.com>
2020-11-12 18:16:57 -0300

    Adiciona classe Contador01

---
+++ Contador01/src/contador01/Contador01.java
@@ -0,0 +1,13 @@
+package contador01;
+
+public class Contador01 {
+
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc < 4) {
+            System.out.println("Cambalhota " + cc);
+            cc++;
+        }
+    }
+
+}
