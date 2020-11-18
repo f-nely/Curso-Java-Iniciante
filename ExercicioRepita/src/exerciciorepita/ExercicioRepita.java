b22096b255974eb342b475d446a34ed3ad3066d5
nely de faria <nelly.nf54@gmail.com>
2020-11-18 16:59:02 -0300

    Adiciona classe ExercicioRepita

---
+++ ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
@@ -0,0 +1,13 @@
+
+package exerciciorepita;
+
+import javax.swing.JOptionPane;
+
+public class ExercicioRepita {
+
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Olá mais uma vez!!", "Boas vindas", JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
